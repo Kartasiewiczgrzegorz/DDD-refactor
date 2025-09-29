f2161674af2ae25749d0ee33bdfe21676f5ce98f
grzegorz-kartasiewicz <kartasiewiczgrzegorz@gmail.com>
2025-09-29 21:49:36 +0200

    security config and failing tests

--- identity-access-management/src/test/java/com/grzegorzkartasiewicz/adapters/UserControllerIT.java
+++ identity-access-management/src/test/java/com/grzegorzkartasiewicz/adapters/UserControllerIT.java
@@ -18,12 +18,15 @@ import com.grzegorzkartasiewicz.domain.Email;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest(UserController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 class UserControllerIT {
 
   @Autowired
